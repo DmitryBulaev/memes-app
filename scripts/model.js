8f6120e3642ff7d165d4093ca3d61cd538992e81
Dmitry Bulaev <bulaevd74@mail.ru>
2025-01-03 10:25:28 +0500

    add mock-data

---
+++ scripts/model.js
@@ -0,0 +1,11 @@
+const PREVIEW_MOCK = {
+  url: "https://i.imgflip.com/9au02y.jpg",
+  textTop: "Когда все пытаются как-то куртиться в этой жизни",
+  textBottom: "А ты просто чилловый парень",
+};
+
+class Model {
+  constructor() {
+    this.preview = PREVIEW_MOCK;
+  }
+}
