0b43e51255cfee5aec0bfdb8104d851a0f3397b8
Dmitry Bulaev <bulaevd74@mail.ru>
2025-01-03 10:26:32 +0500

    init files

---
+++ scripts/controller.js
@@ -0,0 +1,11 @@
+class Controller {
+  constructor() {
+    this.model = new Model();
+    this.view = new View();
+    this.api = new Api();
+  }
+
+  init() {
+    console.log("Инициализация memes app");
+  }
+}
