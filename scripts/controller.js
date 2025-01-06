58bf88f132e73a35c590854598c0f37b52b79ec2
Dmitry Bulaev <bulaevd74@mail.ru>
2025-01-06 18:47:09 +0500

    linked model and view

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,20 +1,35 @@
 class Controller {
   constructor() {
-    this.model = new Model();
-    this.view = new View();
+    this.model = new Model({
+      onCurrentMemeIdChandge: this.handleModelCurrentMemeChange,
+    });
+
+    this.view = new View({
+      onMemeChange: this.handleViewMemeChange,
+    });
+
     this.api = new Api();
   }
 
   init() {
     const memes = this.api.getMemes();
-
     this.model.setMemes(memes);
 
+    this.model.setCurrentMemeId(memes[0].id);
+    const memeId = this.model.getCurrentMemeId();
     const memesList = this.model.getMemes();
+    this.view.renderMemesSelect(memesList, memeId);
+  }
 
-    this.view.renderMemesSelect(memesList);
+  handleViewMemeChange(id) {
+    this.model.setCurrentMemeId(id);
+  }
 
-    const preview = this.model.getPreview();
+  handleModelCurrentMemeChange() {
+    const preview = {
+      ...this.model.getPreview(),
+      url: this.model.getCurrentMeme().url,
+    };
 
     this.view.renderPreview(preview);
   }
