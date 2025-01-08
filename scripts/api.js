70e47204158a963d8d3e8e0c3919e6ef66c91b0a
Dmitry Bulaev <bulaevd74@mail.ru>
2025-01-08 17:55:25 +0500

    added api data

--- scripts/api.js
+++ scripts/api.js
@@ -1,24 +1,11 @@
-const MEMES_MOCK = [
-  {
-    id: "562391530",
-    name: "Chill guy",
-    url: "https://i.imgflip.com/9au02y.jpg",
-    height: 415,
-    width: 415,
-  },
-  {
-    id: "124822590",
-    name: "Left Exit 12 Off Ramp",
-    url: "https://i.imgflip.com/22bdq6.jpg",
-    height: 804,
-    width: 767,
-  },
-];
-
 class Api {
-  constructor() {}
+  constructor() {
+    this.baseUrl = "https://api.imgflip.com";
+  }
 
   getMemes() {
-    return MEMES_MOCK;
+    return fetch(`${this.baseUrl}/get_memes`).then((data) => {
+      return data.json();
+    });
   }
 }
